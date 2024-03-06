68c63e51ba910aa03691c10915d37ec6bfa4668c
GauthamRamesh <gautham.r92@gmail.com>
2024-03-06 15:21:10 +0530

    Added Tests

---
+++ src/test/java/com/ecom/iphoneModule/CreateTest.java
@@ -0,0 +1,10 @@
+package com.ecom.iphoneModule;
+import org.testng.annotations.Test;
+public class CreateTest 
+{
+	@Test
+	public void create()
+	{
+		System.out.println("Created Test!!!!");
+	}
+}
