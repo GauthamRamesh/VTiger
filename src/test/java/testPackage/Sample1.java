3d485902afca3f245b10427282f49c24acd90583
UTHPAL <UTHPAL@DESKTOP-PQG2RVD>
2024-03-06 13:27:53 +0530

    Added Test Scripts

---
+++ src/test/java/testPackage/Sample1.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+public class Sample1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Test Trial 1");
+	}
+
+}
---
+++ src/test/java/testPackage/Sample2.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+public class Sample2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Test Trial 2");
+	}
+
+}
